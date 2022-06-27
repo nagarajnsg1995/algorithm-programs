2120f1db99ee53dc8c3a16a4e1a0708470e3f44e
nagarajnsg1995 <nagarajsbg023@gmail.com>
2022-06-27 20:06:53 +0530

    Added:Bubble sort code

---
+++ src/com/bridgelabz/BubbleSort.java
@@ -0,0 +1,27 @@
+package com.bridgelabz;
+
+
+public class BubbleSort {
+    public static void sortdown(int[] a) {
+        int n = a.length;
+        for (int i = 0; i <= n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (a[i] > a[j]) {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {5, 8, 1, 6, 9, 2, 7};
+        sortdown(a);
+        for (int x : a) {
+            System.out.println(x);
+        }
+    }
+}
+
+
