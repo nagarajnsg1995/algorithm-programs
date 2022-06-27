80477016506f560ad7a5e5f022849ead98c29bc1
nagarajnsg1995 <nagarajsbg023@gmail.com>
2022-06-27 20:18:20 +0530

    Added: Checking the anagram of string

---
+++ src/com/bridgelabz/Anagram.java
@@ -0,0 +1,51 @@
+package com.bridgelabz;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Anagram {
+    public static void main(String[] args) {
+
+        String firstWord, secondWord;
+        Scanner scannerObject = new Scanner(System.in);
+        Anagram anagramObject = new Anagram();
+
+        System.out.println("Enter the first word : ");
+        firstWord = scannerObject.next();
+        System.out.println("Enter the second word : ");
+        secondWord = scannerObject.next();
+        scannerObject.close();
+
+        boolean result = anagramObject.checkIfAnagrams(firstWord, secondWord);
+
+        if (result)
+            System.out.println("The words is anagrams");
+        else
+            System.out.println("The words is not anagrams");
+
+    }
+
+    public boolean checkIfAnagrams(String firstWord, String secondWord) {
+
+        firstWord = firstWord.toLowerCase();
+        secondWord = secondWord.toLowerCase();
+        char[] firstWordArray = firstWord.toCharArray();
+        char[] secondWordArray = secondWord.toCharArray();
+
+        Arrays.sort(firstWordArray);
+        Arrays.sort(secondWordArray);
+        int compareResult = Arrays.compare(firstWordArray, secondWordArray);
+
+        if (compareResult == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
+
+
+
